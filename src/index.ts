01bc000dc0fa398df25ac7eae6fe34ff5ee4179d
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2023-05-03 10:51:47 +0200

    Moving Inbox Locator and CreateInbox to a the CitationRelayService project

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 export * from './ExtractCitationsPlugin';
 export * from './MockExtractCitationsPlugin';
 export * from './SerializeAsPlugin';
-export * from './InboxLocatorPlugin';
-export * from './MockInboxLocatorPlugin';
-export * from './MockCreateInboxPlugin';
